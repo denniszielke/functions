bbf5ed7687f2905354341543700c555157e1c0be
Dennis Zielke <dzielke@microsoft.com>
2017-03-16 11:53:47 +0100

    drei

--- LogicAppTrigger/image.js
+++ LogicAppTrigger/image.js
@@ -22,7 +22,9 @@ module.exports = function(context, req) {
     url:     'https://westus.api.cognitive.microsoft.com/face/v1.0/identify',
     body:    payload
     }, function(error, response, body){
-    console.log(body);
+        console.log(body);
+        context.log("inside callback completed");
+        context.done(); 
     });
 
 };
